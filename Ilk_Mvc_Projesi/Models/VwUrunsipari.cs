b3ceccebf078a30707a8a25e2c7d79ed44d71037
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-12-09 18:49:43 +0300

    North db-first

---
+++ Ilk_Mvc_Projesi/Models/VwUrunsipari.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Ilk_Mvc_Projesi.Models
+{
+    public partial class VwUrunsipari
+    {
+        public string ProductName { get; set; }
+        public decimal? Total { get; set; }
+    }
+}
