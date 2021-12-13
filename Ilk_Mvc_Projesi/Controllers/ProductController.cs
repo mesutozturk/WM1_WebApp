a385b8c9f7690e65b8b28b93ba6ab77f193c7374
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-12-13 15:57:08 +0300

    product1

---
+++ Ilk_Mvc_Projesi/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Ilk_Mvc_Projesi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ilk_Mvc_Projesi.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly NorthwindContext _dbContext;
+
+        public ProductController(NorthwindContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _dbContext.Products
+                .Include(x => x.Category)
+                .Include(x => x.Supplier)
+                .OrderBy(x => x.Category.CategoryName)
+                .ThenBy(x => x.ProductName)
+                .ToList();
+
+            ViewBag.Categories = _dbContext.Categories.OrderBy(x => x.CategoryName).ToList();
+            ViewBag.Suppliers = _dbContext.Suppliers.OrderBy(x => x.CompanyName).ToList();
+
+            return View(model);
+        }
+    }
+}
