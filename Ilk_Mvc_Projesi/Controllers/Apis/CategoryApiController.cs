1de3ba326967e59b199817e0a1f4cd00adadd2b6
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-12-14 16:48:53 +0300

    category api

---
+++ Ilk_Mvc_Projesi/Controllers/Apis/CategoryApiController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Ilk_Mvc_Projesi.Models;
+using Ilk_Mvc_Projesi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ilk_Mvc_Projesi.Controllers.Apis
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CategoryApiController : ControllerBase
+    {
+        private readonly NorthwindContext _dbContext;
+
+        public CategoryApiController(NorthwindContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            try
+            {
+                var categories = _dbContext.Categories
+                    .Include(x=>x.Products)
+                    .OrderBy(x=>x.CategoryName)
+                    .Select(x=> new CategoryViewModel()
+                    {
+                        CategoryId = x.CategoryId,
+                        CategoryName = x.CategoryName,
+                        Description = x.Description,
+                        ProductCount = x.Products.Count
+                    })
+                    .ToList();
+                return Ok(categories);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- Ilk_Mvc_Projesi/Startup.cs
+++ Ilk_Mvc_Projesi/Startup.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Ilk_Mvc_Projesi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
