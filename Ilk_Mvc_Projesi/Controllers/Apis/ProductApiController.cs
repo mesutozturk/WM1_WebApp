9cb7ee7df29cb973ea7aaff2e9f748433d7dd6f6
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-12-20 18:00:12 +0300

    product api ekle ok

---
+++ Ilk_Mvc_Projesi/Controllers/Apis/ProductApiController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Ilk_Mvc_Projesi.Models;
+using Ilk_Mvc_Projesi.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ilk_Mvc_Projesi.Controllers.Apis
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+        private readonly NorthwindContext _dbContext;
+
+        public ProductApiController(NorthwindContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpPost]
+        public IActionResult Add(ProductViewModel model)
+        {
+            var product = new Product()
+            {
+                CategoryId = model.CategoryId,
+                ProductName = model.ProductName,
+                UnitPrice = model.UnitPrice
+            };
+            try
+            {
+                _dbContext.Products.Add(product);
+                _dbContext.SaveChanges();
+                return Ok(new
+                {
+                    Message = "Ürün ekleme işlemi başarılı",
+                    Model = product
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Bir hata oluştu: {ex.Message}");
+            }
+        }
+    }
+}
