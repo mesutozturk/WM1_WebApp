76578d42c395ef1e9c728704cc79f89b4aa51efc
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-12-22 18:58:44 +0300

    mycontext ok

---
+++ ItServiceApp/Models/Identity/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace ItServiceApp.Models.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required, StringLength(50)]
+        [PersonalData]
+        public string Name { get; set; }
+        [Required, StringLength(50)]
+        [PersonalData]
+        public string Surname { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
