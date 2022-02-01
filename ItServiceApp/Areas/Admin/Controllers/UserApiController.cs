708a2003ad90ea8e925f6a2ef47e14189900a9e7
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2022-02-01 16:31:05 +0300

    update users.

--- ItServiceApp/Areas/Admin/Controllers/UserApiController.cs
+++ ItServiceApp/Areas/Admin/Controllers/UserApiController.cs
@@ -1,9 +1,14 @@
-﻿using DevExtreme.AspNet.Data;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DevExtreme.AspNet.Data;
 using ItServiceApp.Extensions;
 using ItServiceApp.Models.Identity;
+using ItServiceApp.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ItServiceApp.Areas.Admin.Controllers
 {
@@ -24,6 +29,25 @@ namespace ItServiceApp.Areas.Admin.Controllers
             var data = _userManager.Users;
 
             return Ok(DataSourceLoader.Load(data, loadOptions));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(string key, string values)
+        {
+            var data = _userManager.Users.FirstOrDefault(x => x.Id == key);
+            if (data == null)
+                return StatusCode(StatusCodes.Status409Conflict, new JsonResponseViewModel()
+                {
+                    IsSuccess = false,
+                    ErrorMessage = "Kullanıcı bulunamadı"
+                });
+
+            JsonConvert.PopulateObject(values, data);
+            if (!TryValidateModel(data))
+                return BadRequest(ModelState.ToFullErrorString());
+
+            var result = await _userManager.UpdateAsync(data);
+            return Ok(new JsonResponseViewModel());
         }
     }
 }
