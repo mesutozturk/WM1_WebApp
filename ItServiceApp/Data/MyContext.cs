46a8d791393f813a7aed0fc1a9eed327efcf31b6
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2022-02-01 19:45:39 +0300

    subscriptions

--- ItServiceApp/Data/MyContext.cs
+++ ItServiceApp/Data/MyContext.cs
@@ -10,6 +10,25 @@ namespace ItServiceApp.Data
         public MyContext(DbContextOptions<MyContext> options)
         : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //Fluent API
+
+            builder.Entity<Subscription>()
+                .Property(x => x.Amount)
+                .HasPrecision(9, 2);
+
+            builder.Entity<Subscription>()
+                .Property(x => x.PaidAmount)
+                .HasPrecision(9, 2);
+
+            builder.Entity<SubscriptionType>()
+                .Property(x => x.Price)
+                .HasPrecision(9, 2);
+
+        }
 
         public DbSet<Address> Addresses { get; set; }
         public DbSet<City> Cities { get; set; }
